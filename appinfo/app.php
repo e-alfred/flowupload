84d8c39f4792f9395ff1dfba28e15a13d4e69112
Nextgen-Networks <reg-github.com@nextgen-networks.de>
2018-09-13 12:03:00 +0200

    show menu entry in NC14
    replaced calls with those from official NC14 deprecation post (https://github.com/nextcloud/server/pull/8811/files/83873e3da1535e02b3eac72e212f7ba0e251b7b9)

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,12 +1,9 @@
 <?php
 
-$l = \OC::$server->getL10N('flowupload');
-$g = \OC::$server->getURLGenerator();
-
 \OC::$server->getNavigationManager()->add(array(
         'id'    => 'flowupload',
         'order' => 74,
-        'href' => $g->linkToRoute('flowupload_index'),
-        'icon' => $g->imagePath('flowupload', 'flowupload.svg'),
+        'href' => OC::$server->getURLGenerator()->linkToRoute('flowupload_index'),
+        'icon' => OC::$server->getURLGenerator()->imagePath('flowupload', 'flowupload.svg'),
         'name' => 'Flowupload'
 ));
