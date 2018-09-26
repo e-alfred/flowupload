e415331e1d1ebb80f50d6ac9e1e0021a5b330cbf
Alfred Egger <alfred.egger@gmx.at>
2018-09-26 16:06:00 +0200

    Fix calls in app.php

--- appinfo/app.php
+++ appinfo/app.php
@@ -3,7 +3,7 @@
 \OC::$server->getNavigationManager()->add(array(
         'id'    => 'flowupload',
         'order' => 74,
-        'href' => OC::$server->getURLGenerator()->linkToRoute('flowupload_index'),
-        'icon' => OC::$server->getURLGenerator()->imagePath('flowupload', 'flowupload.svg'),
+        'href' => \OC::$server->getURLGenerator()->linkToRoute('flowupload_index'),
+        'icon' => \OC::$server->getURLGenerator()->imagePath('flowupload', 'flowupload.svg'),
         'name' => 'Flowupload'
 ));
