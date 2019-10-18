54e153e729d1f4b7271d1373601ac63c83be7517
Jonathan Treffler <trefflerj@web.de>
2019-10-18 22:48:59 +0200

    added UI

--- templates/main.php
+++ templates/main.php
@@ -16,6 +16,7 @@
     <a class="btn btn-small btn-success" ng-click="$flow.resume()"><?= $l->t('Upload/Resume all'); ?></a>
     <a class="btn btn-small btn-danger" ng-click="$flow.pause()"><?= $l->t('Pause'); ?></a>
     <a class="btn btn-small btn-info" ng-click="$flow.cancel()"><?= $l->t('Cancel'); ?></a>
+    <a class="btn btn-small btn-info" ng-click="hideFinished = !hideFinished"><input style="min-height: unset; vertical-align: middle;" type="checkbox" ng-model="hideFinished"></input><span><?= $l->t('Hide finished uploads'); ?></span></a>
     <span class="label label-info"><?= $l->t('Size'); ?>: {{$flow.getSize() | bytes}}</span>
     <span class="label label-info" ng-if="$flow.getFilesCount() != 0"><?= $l->t('Progress'); ?>: {{$flow.progress()*100 | number:2}}%</span>
     <span class="label label-info" ng-if="$flow.isUploading()"><?= $l->t('Uploading'); ?>...</span>
@@ -44,7 +45,7 @@
     </tr>
     </thead>
     <tbody>
-    <tr ng-repeat="file in transfers | orderBy:sortType:sortReverse">
+    <tr ng-if="!(file.isComplete() && hideFinished)" ng-repeat="file in transfers | orderBy:sortType:sortReverse">
       <td>{{$index+1}}</td>
       <td title="UID: {{file.uniqueIdentifier}}">{{file.relativePath}}</td>
       <td title="Chunks: {{file.completeChunks()}} / {{file.chunks.length}}"><span ng-if="file.isUploading()">{{file.size*file.progress() | bytes}}/</span>{{file.size | bytes}}</td>
