80fd5880915e8d4154f1bc75ffe8270bd1fbf07f
Jonathan Treffler <tessy.powder@gmail.com>
2019-11-17 11:39:13 +0100

    Create locations.php
    ... code from NastuzziSamy !!!

---
+++ ajax/locations.php
@@ -0,0 +1,67 @@
+<?php
+  \OCP\User::checkLoggedIn();
+
+  if (!\OCP\App::isEnabled('flowupload')) {
+  	http_response_code(403);
+  }
+
+  function getAllLocations() {
+    // ToDo: Return locations from database
+    return array(
+      array(
+        'id' => 0,
+        'location' => '/flowupload/',
+        'pause' => 0,
+        'uploading' => 0,
+        'completed' => 0,
+        'aborted' => 0
+      )
+    );
+  }
+
+  function addNewLocation($location) {
+    // ToDo: Add to database
+  	$location = preg_replace('/(\.\.\/|~|\/\/)/i', '', '/'.$location.'/');
+    $location = preg_replace('/[^a-z0-9äöüß \(\)\.\-_\/]/i', '', $location);
+  	$location = trim($location);
+
+    return $location;
+  }
+
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_POST = json_decode(file_get_contents('php://input'), true);
+
+    if (isset($_POST['location']) && \OC\Files\Filesystem::isValidPath($_POST['location'])) {
+      $locations = getAllLocations();
+
+      foreach ($locations as $location) {
+        if ($location['location'] === $_POST['location']) {
+          http_response_code(409);
+          die();
+        }
+      }
+
+      http_response_code(201);
+
+      $location = addNewLocation($_POST['location']);
+
+      echo json_encode(array(
+          'id' => preg_replace('#/#', '', $location),
+          'location' => $location,
+          'pause' => 0,
+          'uploading' => 0,
+          'completed' => 0,
+          'aborted' => 0
+        ));
+    }
+    else {
+      http_response_code(400);
+    }
+
+    die();
+  }
+
+  echo json_encode(array(
+    "locations" => getAllLocations()
+  ));
+?>
