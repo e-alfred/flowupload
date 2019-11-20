8579827ec71ffa053f5f0e670ec146c11cfb82ef
Jonathan Treffler <trefflerj@web.de>
2019-11-20 10:19:33 +0100

    integrated js code from NastuzziSamy

--- js/app.js
+++ js/app.js
@@ -21,6 +21,20 @@ app.config(['flowFactoryProvider', function (flowFactoryProvider) {
   });
 }]);
 
+$('li#app-navigation-entry-utils-create').on('click', function() {
+  $('li#app-navigation-entry-utils-add').addClass('editing');
+});
+
+$('li#app-navigation-entry-utils-add .icon-close').on('click', function () {
+  $('li#app-navigation-entry-utils-add').removeClass('editing');
+});
+
+$('li#app-navigation-entry-utils-add .icon-checkmark').on('click', function () {
+  $('li#app-navigation-entry-utils-add').removeClass('editing');
+});
+
+// FILTERS
+
 app.filter('bytes', function() {
 	return function(bytes, precision) {
 		if (isNaN(parseFloat(bytes)) || bytes == 0 || !isFinite(bytes)) return '-';
@@ -51,7 +65,9 @@ app.filter('seconds', function() {
 	};
 });
 
-app.controller('mainController', function($scope,$interval) {
+// CONTROLLERS
+
+app.controller('flow', function($scope,$interval) {
   $scope.sortType     = 'relativePath';
   $scope.sortReverse  = false;
 
@@ -77,4 +93,105 @@ app.controller('mainController', function($scope,$interval) {
   let dynamicTitleInterval = $interval(function() {
     $scope.dynamicTitle();
   },500);
+
+  $scope.locationId = 0;
+
+	$scope.$on('changeLocation', function (event, id, $flow) {
+		$scope.locationId = id;
+		$scope.$flow = $flow;
+
+		console.log(id);
+		console.log($flow);
+	});
+});
+
+app.controller('location', function ($scope) {
+	$scope.init = function (id, name) {
+		$scope.locationId = id;
+		$scope.locationName = name;
+	}
+
+	$scope.seeUploads = function ($event, type) {
+		$event.stopPropagation();
+		$event.preventDefault();
+
+		console.log($scope);
+
+		$('#locations li.active').removeClass('active');
+
+		$($event.currentTarget).addClass('active');
+	};
+
+	$scope.beforeUploading = {
+		query: function (flowFile, flowChunk) {
+			// function will be called for every request
+			console.log('File', flowFile);
+			console.log('Chunk', flowChunk);
+			return {
+				//id: $scope.locationId
+				// Temporary:
+				target: $scope.locationName
+			};
+		}
+	};
+});
+
+app.controller('locations', function ($rootScope, $scope, $http) {
+	$scope.isOpen = false;
+
+	$scope.reloadLocations = function () {
+		setTimeout(function () {
+			if ($scope.locationId === undefined)
+				$($($('.locations')[0]).find('a')).click();
+			else
+				$($('#location-' + $scope.locationId).find('a')).click();
+		}, 100);
+	}
+
+	$scope.setLocation = function (id, $flow) {
+		$rootScope.$broadcast('changeLocation', id, $flow);
+		$scope.$flow = $flow;
+		$scope.locationId = id;
+
+		$('.locations').each(function () {
+			if ($(this).attr('id') === 'location-' + id)
+				$(this).addClass('open');
+			else
+				$(this).removeClass('open');
+			}
+		);
+	}
+
+	$scope.getLocations = function () {
+		$http({
+			method: "GET",
+			url: "ajax/locations.php"
+		}).then(function mySuccess(response) {
+				$scope.locations = response.data.locations;
+
+				$scope.locationId = $scope.locations[0].id;
+			}, function myError(response) {
+				$scope.locations = {};
+			}
+		);
+	};
+
+	$scope.addNewLocation = function () {
+		$http({
+			method : "POST",
+			url : "ajax/locations.php",
+			data : {
+				location: $('#newLocationName').val()
+			}
+		}).then(function mySuccess(response) {
+			$('#newLocationName').val('');
+
+			$scope.locationId = response.data.id;
+			$scope.locations.push(response.data);
+		}, function myError(response) {
+			$scope.locations = {};
+		});
+	};
+
+	$scope.getLocations();
 });
--- templates/main.php
+++ templates/main.php
@@ -1,6 +1,7 @@
-<div ng-app="app" flow-init id="app" ng-controller="mainController" flow-drop flow-drag-enter="class='file-drag'" flow-drag-leave="class=''" ng-class="class">
+<div ng-app="app" flow-init id="app" flow-drop flow-drag-enter="class='file-drag'" flow-drag-leave="class=''" ng-class="class">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-  
+
+  <!-- APP NAVIAGTION -->
   <div id="app-navigation">
     <ul id="locations" class="with-icon" ng-controller="locations">
       <li flow-prevent-drop flow-drop flow-drag-enter="class='alert-success'" flow-drag-leave="class=''" ng-class="class" ng-controller="location" flow-init="init(location.id, location.location); beforeUploading" ng-style="style" ng-repeat="location in locations" ng-init="$last && reloadLocations()" id="location-{{location.id}}" class="collapsible locations">
@@ -32,123 +33,126 @@
 
     <input id="currentLocation" type="hidden" />
   </div>
-  
-  <h2 id="title"><?= $l->t('Transfers'); ?></h2>
 
-  <div class="buttonGroup">
-    <span class="button" flow-btn>
-      <span class="icon icon-file select-file-icon" style=""></span>
-      <span><?= $l->t('Select File'); ?></span>
-    </span>
-    <span class="button" flow-btn flow-directory ng-show="$flow.supportDirectory">
-      <span class="icon icon-files" style="background-image: var(--icon-files-000);"></span>
-      <span><?= $l->t('Select Folder'); ?></span>
-    </span>
-  </div>
+  <!-- MAIN -->
+  <div id="main" ng-controller="flow">
+    <h2 id="title"><?= $l->t('Transfers'); ?></h2>
 
-  <hr>
+    <div class="buttonGroup">
+      <span class="button" flow-btn>
+        <span class="icon icon-file select-file-icon" style=""></span>
+        <span><?= $l->t('Select File'); ?></span>
+      </span>
+      <span class="button" flow-btn flow-directory ng-show="$flow.supportDirectory">
+        <span class="icon icon-files" style="background-image: var(--icon-files-000);"></span>
+        <span><?= $l->t('Select Folder'); ?></span>
+      </span>
+    </div>
 
-  <div class="buttonGroup">
-    <a class="button" ng-click="$flow.resume()">
-        <span class="icon icon-play"></span>
-        <span><?= $l->t('Start/Resume'); ?></span>
-    </a>
-    <a class="button" ng-click="$flow.pause()">
-        <span class="icon icon-pause"></span>
-        <span><?= $l->t('Pause'); ?></span>
-    </a>
-    <a class="button" ng-click="$flow.cancel()">
-        <span class="icon icon-close"></span>
-        <span><?= $l->t('Cancel'); ?></span>
-    </a>
-    <a id="hideFinishedButton" class="button" ng-click="hideFinished = !hideFinished">
-      <input type="checkbox" ng-model="hideFinished"></input>
-      <span><?= $l->t('Hide finished uploads'); ?></span>
-    </a>
-  </div>
+    <hr>
 
-  <hr>
+    <div class="buttonGroup">
+      <a class="button" ng-click="$flow.resume()">
+          <span class="icon icon-play"></span>
+          <span><?= $l->t('Start/Resume'); ?></span>
+      </a>
+      <a class="button" ng-click="$flow.pause()">
+          <span class="icon icon-pause"></span>
+          <span><?= $l->t('Pause'); ?></span>
+      </a>
+      <a class="button" ng-click="$flow.cancel()">
+          <span class="icon icon-close"></span>
+          <span><?= $l->t('Cancel'); ?></span>
+      </a>
+      <a id="hideFinishedButton" class="button" ng-click="hideFinished = !hideFinished">
+        <input type="checkbox" ng-model="hideFinished"></input>
+        <span><?= $l->t('Hide finished uploads'); ?></span>
+      </a>
+    </div>
 
-  <p>
-    <span class="label"><?= $l->t('Size'); ?>: {{$flow.getSize() | bytes}}</span>
-    <span class="label" ng-if="$flow.getFilesCount() != 0"><?= $l->t('Progress'); ?>: {{$flow.progress()*100 | number:2}}%</span>
-     <span class="label" ng-if="$flow.isUploading()"><?= $l->t('Time remaining'); ?>: {{$flow.timeRemaining() | seconds}}</span>
-    <span class="label" ng-if="$flow.isUploading()"><?= $l->t('Uploading'); ?>...</span>
-  </p>
+    <hr>
 
-  <hr>
+    <p>
+      <span class="label"><?= $l->t('Size'); ?>: {{$flow.getSize() | bytes}}</span>
+      <span class="label" ng-if="$flow.getFilesCount() != 0"><?= $l->t('Progress'); ?>: {{$flow.progress()*100 | number:2}}%</span>
+       <span class="label" ng-if="$flow.isUploading()"><?= $l->t('Time remaining'); ?>: {{$flow.timeRemaining() | seconds}}</span>
+      <span class="label" ng-if="$flow.isUploading()"><?= $l->t('Uploading'); ?>...</span>
+    </p>
 
-  <table id="uploadsTable" flow-transfers>
-    <thead>
-    <tr>
-      <th class="hideOnMobile" style="width:5%">
-        <span class="noselect">#</span>
-      </th>
-      <th ng-click="tableSortClicked('relativePath')">
-        <a class="noselect">
-          <span><?= htmlspecialchars($l->t('Name')); ?></span>
-          <span ng-class="{'icon-triangle-n':  (sortType == 'relativePath' && sortReverse), 'icon-triangle-s': (sortType == 'relativePath' && !sortReverse)}" class="sortIndicator"></span>
-        </a>
-      </th>
-      <th></th>
-      <th class="hideOnMobile" ng-click="tableSortClicked('-currentSpeed')" style="width:10%">
-        <a class="noselect">
-          <span><?= htmlspecialchars($l->t('Uploadspeed')); ?></span>
-          <span ng-class="{'icon-triangle-n':  (sortType == '-currentSpeed' && sortReverse), 'icon-triangle-s': (sortType == '-currentSpeed' && !sortReverse)}" class="sortIndicator"></span>
-        </a>
-      </th>
-      <th ng-click="tableSortClicked('-size')" style="width:10%">
-        <a class="noselect">
-          <span><?= htmlspecialchars($l->t('Size')); ?></span>
-          <span ng-class="{'icon-triangle-n':  (sortType == '-size' && sortReverse), 'icon-triangle-s': (sortType == '-size' && !sortReverse)}" class="sortIndicator"></span>
-        </a>
-      </th>
-      <th ng-click="tableSortClicked('-progress()')" style="width:20%">
-        <a class="noselect">
-          <span><?= htmlspecialchars($l->t('Progress')); ?></span>
-          <span ng-class="{'icon-triangle-n':  (sortType == '-progress()' && sortReverse), 'icon-triangle-s': (sortType == '-progress()' && !sortReverse)}" class="sortIndicator"></span>
-        </a>
-      </th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr ng-if="!(file.isComplete() && hideFinished)" ng-repeat="file in transfers | orderBy:sortType:sortReverse">
-      <td class="hideOnMobile">{{$index+1}}</td>
-      <td class="ellipsis" title="UID: {{file.uniqueIdentifier}}">
-          <span>{{file.relativePath}}</span>
-      </td>
-      <td>
-        <div class="actions" ng-if="!file.isComplete() || file.error">
-          <a class="action permanent" title="<?= htmlspecialchars($l->t('Resume')); ?>" ng-click="file.resume()" ng-if="!file.isUploading() && !file.error">
-            <span class="icon icon-play"></span>
+    <hr>
+
+    <table id="uploadsTable" flow-transfers>
+      <thead>
+      <tr>
+        <th class="hideOnMobile" style="width:5%">
+          <span class="noselect">#</span>
+        </th>
+        <th ng-click="tableSortClicked('relativePath')">
+          <a class="noselect">
+            <span><?= htmlspecialchars($l->t('Name')); ?></span>
+            <span ng-class="{'icon-triangle-n':  (sortType == 'relativePath' && sortReverse), 'icon-triangle-s': (sortType == 'relativePath' && !sortReverse)}" class="sortIndicator"></span>
           </a>
-          <a class="action permanent" title="<?= htmlspecialchars($l->t('Pause')); ?>" ng-click="file.pause()" ng-if="file.isUploading() && !file.error">
-            <span class="icon icon-pause"></span>
+        </th>
+        <th></th>
+        <th class="hideOnMobile" ng-click="tableSortClicked('-currentSpeed')" style="width:10%">
+          <a class="noselect">
+            <span><?= htmlspecialchars($l->t('Uploadspeed')); ?></span>
+            <span ng-class="{'icon-triangle-n':  (sortType == '-currentSpeed' && sortReverse), 'icon-triangle-s': (sortType == '-currentSpeed' && !sortReverse)}" class="sortIndicator"></span>
           </a>
-          <a class="action permanent" title="<?= htmlspecialchars($l->t('Retry')); ?>" ng-click="file.retry()" ng-show="file.error">
-            <span class="icon icon-play"></span>
+        </th>
+        <th ng-click="tableSortClicked('-size')" style="width:10%">
+          <a class="noselect">
+            <span><?= htmlspecialchars($l->t('Size')); ?></span>
+            <span ng-class="{'icon-triangle-n':  (sortType == '-size' && sortReverse), 'icon-triangle-s': (sortType == '-size' && !sortReverse)}" class="sortIndicator"></span>
           </a>
-          <a class="action permanent" title="<?= htmlspecialchars($l->t('Cancel')); ?>" ng-click="file.cancel()">
-            <span class="icon icon-close"></span>
+        </th>
+        <th ng-click="tableSortClicked('-progress()')" style="width:20%">
+          <a class="noselect">
+            <span><?= htmlspecialchars($l->t('Progress')); ?></span>
+            <span ng-class="{'icon-triangle-n':  (sortType == '-progress()' && sortReverse), 'icon-triangle-s': (sortType == '-progress()' && !sortReverse)}" class="sortIndicator"></span>
           </a>
-        </div>
-      </td>
-      <td class="hideOnMobile">
-          <span ng-if="file.isUploading()">{{file.currentSpeed | byterate}}</span>
-      </td>
-      <td title="Chunks: {{file.completeChunks()}} / {{file.chunks.length}}">
-          <span class="hideOnMobile" ng-if="!file.isComplete()">{{file.size*file.progress() | bytes}}/</span>
-          <span>{{file.size | bytes}}</span>
-      </td>
-      <td>
-        <progress ng-if="!file.isComplete() && !file.error" class="progressbar hideOnMobile" max="1" value="{{file.progress()}}"></progress>
-        <span ng-if="!file.isComplete() && !file.error">{{file.progress()*100 | number:2}}%</span>
-        <span ng-if="file.isComplete() && !file.error"><?= htmlspecialchars($l->t('Completed')); ?></span>
-        <span ng-if="file.error"><?= htmlspecialchars($l->t('Error')); ?></span>
-      </td>
-    </tr>
-    </tbody>
-  </table>
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr ng-if="!(file.isComplete() && hideFinished)" ng-repeat="file in transfers | orderBy:sortType:sortReverse">
+        <td class="hideOnMobile">{{$index+1}}</td>
+        <td class="ellipsis" title="UID: {{file.uniqueIdentifier}}">
+            <span>{{file.relativePath}}</span>
+        </td>
+        <td>
+          <div class="actions" ng-if="!file.isComplete() || file.error">
+            <a class="action permanent" title="<?= htmlspecialchars($l->t('Resume')); ?>" ng-click="file.resume()" ng-if="!file.isUploading() && !file.error">
+              <span class="icon icon-play"></span>
+            </a>
+            <a class="action permanent" title="<?= htmlspecialchars($l->t('Pause')); ?>" ng-click="file.pause()" ng-if="file.isUploading() && !file.error">
+              <span class="icon icon-pause"></span>
+            </a>
+            <a class="action permanent" title="<?= htmlspecialchars($l->t('Retry')); ?>" ng-click="file.retry()" ng-show="file.error">
+              <span class="icon icon-play"></span>
+            </a>
+            <a class="action permanent" title="<?= htmlspecialchars($l->t('Cancel')); ?>" ng-click="file.cancel()">
+              <span class="icon icon-close"></span>
+            </a>
+          </div>
+        </td>
+        <td class="hideOnMobile">
+            <span ng-if="file.isUploading()">{{file.currentSpeed | byterate}}</span>
+        </td>
+        <td title="Chunks: {{file.completeChunks()}} / {{file.chunks.length}}">
+            <span class="hideOnMobile" ng-if="!file.isComplete()">{{file.size*file.progress() | bytes}}/</span>
+            <span>{{file.size | bytes}}</span>
+        </td>
+        <td>
+          <progress ng-if="!file.isComplete() && !file.error" class="progressbar hideOnMobile" max="1" value="{{file.progress()}}"></progress>
+          <span ng-if="!file.isComplete() && !file.error">{{file.progress()*100 | number:2}}%</span>
+          <span ng-if="file.isComplete() && !file.error"><?= htmlspecialchars($l->t('Completed')); ?></span>
+          <span ng-if="file.error"><?= htmlspecialchars($l->t('Error')); ?></span>
+        </td>
+      </tr>
+      </tbody>
+    </table>
 
-  <p id="homeDirectoryLink"><a href="../files?dir=%2Fflowupload"><?= htmlspecialchars($l->t('The files will be saved in your home directory.')); ?></a></p>
+    <p id="homeDirectoryLink"><a href="../files?dir=%2Fflowupload"><?= htmlspecialchars($l->t('The files will be saved in your home directory.')); ?></a></p>
+  <div>
  </div>
