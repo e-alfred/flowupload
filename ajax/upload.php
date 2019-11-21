3d2548cf4147ad9aa40e3af5af15de89a0a601d9
Jonathan Treffler <tessy.powder@gmail.com>
2019-11-21 19:01:11 +0100

    renamed path to fileRelativePath

--- ajax/upload.php
+++ ajax/upload.php
@@ -19,19 +19,21 @@ $config = new \Flow\Config();
 $config->setTempDir($temp);
 $request = new \Flow\Request();
 
+$fileRelativePath = $request->getRelativePath();
+
 // Filter paths
-$path = preg_replace('/(\.\.\/|~|\/\/)/i', '', $request->getRelativePath());
-$path = html_entity_decode(htmlentities($path, ENT_QUOTES, 'UTF-8'));
-$path = trim($path, '/');
+$fileRelativePath = preg_replace('/(\.\.\/|~|\/\/)/i', '', $fileRelativePath);
+$fileRelativePath = html_entity_decode(htmlentities($fileRelativePath, ENT_QUOTES, 'UTF-8'));
+$fileRelativePath = trim($fileRelativePath, '/');
 
 // Skip existing files // ToDo: Check if file size changed?
-if (\OC\Files\Filesystem::file_exists($uploadTarget . $path)) {
+if (\OC\Files\Filesystem::file_exists($uploadTarget . $fileRelativePath)) {
 	http_response_code(200);
 	die();
 }
 
 // Process upload
-if (\OC\Files\Filesystem::isValidPath($path)) {
+if (\OC\Files\Filesystem::isValidPath($fileRelativePath)) {
 
 	// Create temporary upload folder
 	if(!file_exists($temp)) {
@@ -39,26 +41,26 @@ if (\OC\Files\Filesystem::isValidPath($path)) {
 	}
 
 	// Create destination directory
-	$dir = dirname($uploadTarget . $path);
+	$dir = dirname($uploadTarget . $fileRelativePath);
 	if(!\OC\Files\Filesystem::file_exists($dir)) {
 		\OC\Files\Filesystem::mkdir($dir);
 	}
 
 	// Store file
-	if (\Flow\Basic::save($userhome . "/files/" . $uploadTarget . $path, $config, $request)) {
+	if (\Flow\Basic::save($userhome . "/files/" . $uploadTarget . $fileRelativePath, $config, $request)) {
 
                 // no real copy, file comes from somewhere else, e.g. version rollback -> just update the file cache and the webdav properties without all the other post_write actions
-//                \OC\Files\Cache\Cache::checkUpdate($uploadTarget . $path);
-//                \OC\Files\Filesystem::removeETagHook(array("path" => $uploadTarget . $path));
+//                \OC\Files\Cache\Cache::checkUpdate($uploadTarget . $fileRelativePath);
+//                \OC\Files\Filesystem::removeETagHook(array("path" => $uploadTarget . $fileRelativePath));
 
 
 		OC_Hook::emit(
 			\OC\Files\Filesystem::CLASSNAME,
 			\OC\Files\Filesystem::signal_post_write,
-			array( \OC\Files\Filesystem::signal_param_path => $uploadTarget . $path)
+			array( \OC\Files\Filesystem::signal_param_path => $uploadTarget . $fileRelativePath)
 		);
 
-		\OC\Files\Filesystem::touch($uploadTarget . $path);
+		\OC\Files\Filesystem::touch($uploadTarget . $fileRelativePath);
 
 	} else {
 		// This is not a final chunk or request is invalid, continue to upload.
@@ -70,5 +72,5 @@ if (\OC\Files\Filesystem::isValidPath($path)) {
 
 // ToDo: error handling
 //OCP\JSON::error(array("data" => array("message" => $msg)));
-//OCP\Util::writeLog('flowupload', "Failed to create file: " . $path, OC_Log::ERROR);
+//OCP\Util::writeLog('flowupload', "Failed to create file: " . $fileRelativePath, OC_Log::ERROR);
 ?>
