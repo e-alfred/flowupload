05534ea8ba0150b7f605220132596c438c5e94da
Jonathan Treffler <tessy.powder@gmail.com>
2019-11-23 16:00:14 +0100

    upload.php cleanup
    (cleanup not yet finished)

--- ajax/upload.php
+++ ajax/upload.php
@@ -1,7 +1,9 @@
 <?php
 // Restrict access
 if (!\OC::$server->getUserSession()->isLoggedIn()) {
-  http_response_code(403);
+    echo("not logged in \n");
+    http_response_code(403);
+    die();
 }
 
 // Load upload classes
@@ -12,13 +14,12 @@ Flow\Autoloader::register();
 $userhome = OC_User::getHome(\OC::$server->getUserSession()->getUser()->getUID());
 $temp = $userhome.'/.flowupload_tmp/';
 $uploadTarget = $_REQUEST['target'] ?? '/flowupload/';
-$uploadTarget = '/'.$uploadTarget.'/';
+//$uploadTarget = '/'.$uploadTarget.'/';
 
 // Initialize uploader
 $config = new \Flow\Config();
 $config->setTempDir($temp);
 $request = new \Flow\Request();
-
 $fileRelativePath = $request->getRelativePath();
 
 // Filter paths
@@ -28,48 +29,42 @@ $fileRelativePath = trim($fileRelativePath, '/');
 
 // Skip existing files // ToDo: Check if file size changed?
 if (\OC\Files\Filesystem::file_exists($uploadTarget . $fileRelativePath)) {
+    echo("file already exists \n");
 	http_response_code(200);
 	die();
 }
 
-// Process upload
-if (\OC\Files\Filesystem::isValidPath($fileRelativePath)) {
-
-	// Create temporary upload folder
-	if(!file_exists($temp)) {
-		mkdir($temp);
-	}
-
-	// Create destination directory
-	$dir = dirname($uploadTarget . $fileRelativePath);
-	if(!\OC\Files\Filesystem::file_exists($dir)) {
-		\OC\Files\Filesystem::mkdir($dir);
-	}
-
-	// Store file
-	if (\Flow\Basic::save($userhome . "/files/" . $uploadTarget . $fileRelativePath, $config, $request)) {
-
-                // no real copy, file comes from somewhere else, e.g. version rollback -> just update the file cache and the webdav properties without all the other post_write actions
-//                \OC\Files\Cache\Cache::checkUpdate($uploadTarget . $fileRelativePath);
-//                \OC\Files\Filesystem::removeETagHook(array("path" => $uploadTarget . $fileRelativePath));
-
-
-		OC_Hook::emit(
-			\OC\Files\Filesystem::CLASSNAME,
-			\OC\Files\Filesystem::signal_post_write,
-			array( \OC\Files\Filesystem::signal_param_path => $uploadTarget . $fileRelativePath)
-		);
-
-		\OC\Files\Filesystem::touch($uploadTarget . $fileRelativePath);
-
-	} else {
-		// This is not a final chunk or request is invalid, continue to upload.
-	}
-
-	// Remove old chunks
-	\Flow\Uploader::pruneChunks($temp);
+// check if path is valid
+if (!\OC\Files\Filesystem::isValidPath($uploadTarget . $fileRelativePath)) {
+    http_response_code(403);
+    die();
 }
 
+// Create temporary upload folder
+if(!file_exists($temp)) {
+	mkdir($temp);
+}
+
+// Create destination directory
+$dir = dirname($uploadTarget . $fileRelativePath);
+if(!\OC\Files\Filesystem::file_exists($dir)) {
+	\OC\Files\Filesystem::mkdir($dir);
+}
+
+// Store file
+if (\Flow\Basic::save($userhome . "/files/" . $uploadTarget . $fileRelativePath, $config, $request)) {
+	OC_Hook::emit(
+		\OC\Files\Filesystem::CLASSNAME,
+		\OC\Files\Filesystem::signal_post_write,
+		array( \OC\Files\Filesystem::signal_param_path => $uploadTarget . $fileRelativePath)
+	);
+	\OC\Files\Filesystem::touch($uploadTarget . $fileRelativePath);
+} else {
+	// This is not a final chunk or request is invalid, continue to upload.
+}
+// Remove old chunks
+\Flow\Uploader::pruneChunks($temp);
+	
 // ToDo: error handling
 //OCP\JSON::error(array("data" => array("message" => $msg)));
 //OCP\Util::writeLog('flowupload', "Failed to create file: " . $fileRelativePath, OC_Log::ERROR);
