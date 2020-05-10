bfa8487c2aafe017cb95df9722be6d320b605665
TessyPowder <tessy.powder@gmail.com>
2020-05-10 14:56:33 +0200

    now using the vue libary for the App Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,56 @@
 <template>
-    <div id="app" role="main">
+    <Content :class="{'icon-loading': loading}" app-name="flowupload">
     	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     	<!-- APP NAVIAGTION -->
-    	<div id="app-navigation">
-    		<div class="app-navigation-new">
+    	<AppNavigation>
+    		<!--<div class="app-navigation-new">
     			<ul>
     				<li id="app-navigation-entry-utils-create" v-on:click="pickNewLocation()" class="app-navigation-entry-utils-menu-button">
     					<button class="icon-add">{{ t('flowupload', 'New destination') }}></button>
     				</li>
     			</ul>
-    		</div>
+    		</div>-->
+    		<AppNavigationNew
+				:text="t('flowupload', 'New destination')"
+				:disabled="false"
+				button-id="new-location-button"
+				button-class="icon-add"
+				v-on:click="pickNewLocation()" />
     		<ul id="locations" class="with-icon">
-    			<li class="fileDropZone" v-for="location in locations" v-bind:key="location.id" v-bind:id="'location-' + location.path">
+    		    <AppNavigationItem icon="icon-folder" v-bind:title="location.path" class="fileDropZone" v-for="location in locations" v-bind:key="location.id" v-bind:id="'location-' + location.path">
+					<AppNavigationCounter slot="counter" :highlighted="true">{{ location.flow.files.length }}</AppNavigationCounter>
+					<template slot="actions">
+						<ActionButton icon="icon-edit" v-bind:href="'/index.php/apps/files/?dir=' + location.path">
+							{{ t('flowupload', 'Open') }}
+						</ActionButton>
+						<ActionButton icon="icon-starred" @click="toggleStarredLocation(location.path)">
+							{{ t('flowupload', 'Star') }}
+						</ActionButton>
+						<ActionButton icon="icon-delete" @click="removeLocation(location.path)">
+							{{ t('flowupload', 'Remove') }}
+						</ActionButton>
+					</template>
+				</AppNavigationItem>
+    			<!--<li>
     				<a ng-href="" class="icon-folder" v-on:click="switchActiveLocationById(location.id)" v-bind:title="location.path">{{location.path}}</a>
     				<div class="app-navigation-entry-utils">
     					<ul>
-    						<li class="app-navigation-entry-utils-counter" v-bind:title="location.flow.files.length + ' ' + t('flowupload', 'Files')">{{ location.flow.files.length }}</li>
+    						<li class="app-navigation-entry-utils-counter" v-bind:title="location.flow.files.length + ' ' + t('flowupload', 'Files')"></li>
     						<li class="app-navigation-entry-utils-menu-button"><button></button></li>
     					</ul>
     				</div>
     				<div appNavigationEntryMenu>
     					<ul>
     						<li>
-    							<a v-bind:href="'/index.php/apps/files/?dir=' + location.path" target="_blank" rel="noopener noreferrer">
+    							<a v-bind:href="" target="_blank" rel="noopener noreferrer">
     							<span class="icon-files"></span>
-    							<span>{{ t('flowupload', 'Open') }}</span>
+    							<span>{{  }}</span>
     							</a>
     						</li>
-    						<li v-on:click="toggleStarredLocation(location.path)">
+    						<li v-on:click="">
     							<a href="">
     							<span class="icon-starred"></span>
-    							<span v-if="!location.starred">{{ t('flowupload', 'Star') }}</span>
+    							<span v-if="!location.starred">{{  }}</span>
     							<span v-if="location.starred">{{ t('flowupload', 'Unstar') }}</span>
     							</a>
     						</li>
@@ -42,9 +62,9 @@
     						</li>
     					</ul>
     				</div>
-    			</li>
+    			</li>-->
     		</ul>
-    		<div id="app-settings">
+    		<!--<div id="app-settings">
     			<div id="app-settings-header">
     				<button class="settings-button"
     					data-apps-slide-toggle="#app-settings-content">
@@ -53,129 +73,134 @@
     			</div>
     			<div id="app-settings-content">
     				<!-- Your settings content here -->
-    			</div>
-    		</div>
-    	</div>
+    			<!--</div>
+    		</div>-->
+    		<AppNavigationSettings>
+    		    test
+    		</AppNavigationSettings>
+    	</AppNavigation>
     	
     	<!-- CONTENT -->
-    	<div class="fileDropZone" id="app-content" style="padding: 2.5%; width:auto" v-if="activeLocation.id != false">
-    		<div id="noLocationSelected" v-show="activeLocation === undefined && loaded">{{ t('flowupload', 'Please select a location') }}</div>
-    		<div id="locationSelected" ng-cloak v-show="activeLocation != undefined">
-    			<h2 id="title">{{ t('flowupload', 'Transfers') }}</h2>
-    			<div class="buttonGroup">
-    				<span class="uploadSelectButton button" uploadtype="file">
-    				<span class="icon icon-file select-file-icon" style=""></span>
-    				<span>{{ t('flowupload', 'Select File') }}</span>
-    				</span>
-    				<input id="FileSelectInput" type="file" multiple="multiple">
-    				<span class="uploadSelectButton button" uploadtype="folder" v-show="activeLocation.flow.supportDirectory">
-    				<span class="icon icon-files" style="background-image: var(--icon-files-000);"></span>
-    				<span>{{ t('flowupload', 'Select Folder') }}</span>
-    				</span>
-    				<input id="FolderSelectInput" type="file" multiple="multiple" webkitdirectory="webkitdirectory">
-    			</div>
-    			<hr>
-    			<div class="buttonGroup">
-    				<a class="button" v-on:click="activeLocation.flow.resume()">
-    				    <span class="icon icon-play"></span>
-    				    <span>{{ t('flowupload', 'Start/Resume') }}</span>
-    				</a>
-    				<a class="button" v-on:click="activeLocation.flow.pause()">
-    				    <span class="icon icon-pause"></span>
-    				    <span>{{ t('flowupload', 'Pause') }}</span>
-    				</a>
-    				<a class="button" v-on:click="activeLocation.flow.cancel()">
-    				    <span class="icon icon-close"></span>
-    				<span>{{ t('flowupload', 'Cancel') }}</span>
-    				</a>
-    				<a id="hideFinishedButton" class="button" v-on:click="hideFinished = !hideFinished">
-    				    <input type="checkbox" ng-model="hideFinished"></input>
-    				    <span>{{ t('flowupload', 'Hide finished uploads') }}</span>
-    				</a>
-    			</div>
-    			<hr>
-    			<p>
-    				<span class="label">{{ t('flowupload', 'Size') + ' : ' /*+ activeLocation.flow.getSize() | bytes*/ }}</span>
-    				<span class="label" v-if="activeLocation.flow.getFilesCount() != 0">{{ t('flowupload', 'Progress') + ' : ' + trimDecimals(activeLocation.flow.progress()*100, 2) + '%'}}</span>
-    				<span class="label" v-if="activeLocation.flow.isUploading()">{{ t('flowupload', 'Time remaining') + ' : ' + activeLocation.flow.timeRemaining() | seconds }}</span>
-    				<span class="label" v-if="activeLocation.flow.isUploading()">{{ t('flowupload', 'Uploading') + '...' }}</span>
-    			</p>
-    			<hr>
-    			<table id="uploadsTable">
-    				<thead>
-    					<tr>
-    						<th class="hideOnMobile" style="width:5%">
-    							<span class="noselect">#</span>
-    						</th>
-    						<th ng-click="tableSortClicked('relativePath')">
-    							<a class="noselect">
-    							<span>{{ t('flowupload', 'Name') }}</span>
-    							<span ng-class="{'icon-triangle-n':  (sortType == 'relativePath' && sortReverse), 'icon-triangle-s': (sortType == 'relativePath' && !sortReverse)}" class="sortIndicator"></span>
-    							</a>
-    						</th>
-    						<th></th>
-    						<th class="hideOnMobile" ng-click="tableSortClicked('-currentSpeed')" style="width:10%">
-    							<a class="noselect">
-    							<span>{{ t('flowupload', 'Uploadspeed') }}</span>
-    							<span ng-class="{'icon-triangle-n':  (sortType == '-currentSpeed' && sortReverse), 'icon-triangle-s': (sortType == '-currentSpeed' && !sortReverse)}" class="sortIndicator"></span>
-    							</a>
-    						</th>
-    						<th ng-click="tableSortClicked('-size')" style="width:10%">
-    							<a class="noselect">
-    							<span>{{ t('flowupload', 'Size') }}</span>
-    							<span ng-class="{'icon-triangle-n':  (sortType == '-size' && sortReverse), 'icon-triangle-s': (sortType == '-size' && !sortReverse)}" class="sortIndicator"></span>
-    							</a>
-    						</th>
-    						<th ng-click="tableSortClicked('-progress()')" style="width:20%">
-    							<a class="noselect">
-    							<span>{{ t('flowupload', 'Progress') }}</span>
-    							<span ng-class="{'icon-triangle-n':  (sortType == '-progress()' && sortReverse), 'icon-triangle-s': (sortType == '-progress()' && !sortReverse)}" class="sortIndicator"></span>
-    							</a>
-    						</th>
-    					</tr>
-    				</thead>
-    				<tbody>
-    					<tr v-if="!(file.isComplete() && hideFinished)" v-for="file in filteredFiles">
-    						<td class="hideOnMobile">{{$index+1}}</td>
-    						<td class="ellipsis" v-bind:title="'UID: ' + file.uniqueIdentifier">
-    							<span>{{file.relativePath}}</span>
-    						</td>
-    						<td>
-    							<div class="actions" v-if="!file.isComplete() || file.error">
-    								<a class="action permanent" v-bind:title="t('flowupload', 'Resume')" v-click="file.resume()" v-if="!file.isUploading() && !file.error">
-    								<span class="icon icon-play"></span>
-    								</a>
-    								<a class="action permanent" v-bind:title="t('flowupload', 'Pause')" v-click="file.pause()" v-if="file.isUploading() && !file.error">
-    								<span class="icon icon-pause"></span>
-    								</a>
-    								<a class="action permanent" v-bind:title="t('flowupload', 'Retry')" v-click="file.retry()" v-show="file.error">
-    								<span class="icon icon-play"></span>
-    								</a>
-    								<a class="action permanent" v-bind:title="t('flowupload', 'Cancel')" v-click="file.cancel()">
-    								<span class="icon icon-close"></span>
-    								</a>
-    							</div>
-    						</td>
-    						<td class="hideOnMobile">
-    							<span v-if="file.isUploading()">{{file.currentSpeed | byterate}}</span>
-    						</td>
-    						<td title="'Chunks: ' + file | completedChunks + ' / ' + file.chunks.length">
-    							<span class="hideOnMobile" v-if="!file.isComplete()">{{file.size*file.progress() | bytes}}/</span>
-    							<span>{{file.size | bytes}}</span>
-    						</td>
-    						<td>
-    							<progress v-if="!file.isComplete() && !file.error" class="progressbar hideOnMobile" max="1" v-bind:value="file.progress()"></progress>
-    							<span v-if="!file.isComplete() && !file.error">{{ trimDecimals(file.progress()*100, 2) }}%</span>
-    							<span v-if="file.isComplete() && !file.error">{{ t('flowupload', 'Completed') }}</span>
-    							<span v-if="file.error">{{ t('flowupload', 'Error') }}</span>
-    						</td>
-    					</tr>
-    				</tbody>
-    			</table>
-    			<p id="homeDirectoryLink"><a href="../files?dir=%2Fflowupload">{{ t('flowupload', 'The files will be saved in your home directory.') }}</a></p>
-    		</div>
-    	</div>
-    </div>
+    	<AppContent>
+        	<div class="fileDropZone" style="padding: 2.5%; width:auto" v-if="!loading">
+        		<div id="noLocationSelected" v-show="activeLocation === undefined && loaded">{{ t('flowupload', 'Please select a location') }}</div>
+        		<div id="locationSelected" ng-cloak v-show="activeLocation != undefined">
+        			<h2 id="title">{{ t('flowupload', 'Transfers') }}</h2>
+        			<div class="buttonGroup">
+        				<span class="uploadSelectButton button" uploadtype="file">
+        				<span class="icon icon-file select-file-icon" style=""></span>
+        				<span>{{ t('flowupload', 'Select File') }}</span>
+        				</span>
+        				<input id="FileSelectInput" type="file" multiple="multiple">
+        				<span class="uploadSelectButton button" uploadtype="folder" v-show="activeLocation.flow.supportDirectory">
+        				<span class="icon icon-files" style="background-image: var(--icon-files-000);"></span>
+        				<span>{{ t('flowupload', 'Select Folder') }}</span>
+        				</span>
+        				<input id="FolderSelectInput" type="file" multiple="multiple" webkitdirectory="webkitdirectory">
+        			</div>
+        			<hr>
+        			<div class="buttonGroup">
+        				<a class="button" v-on:click="activeLocation.flow.resume()">
+        				    <span class="icon icon-play"></span>
+        				    <span>{{ t('flowupload', 'Start/Resume') }}</span>
+        				</a>
+        				<a class="button" v-on:click="activeLocation.flow.pause()">
+        				    <span class="icon icon-pause"></span>
+        				    <span>{{ t('flowupload', 'Pause') }}</span>
+        				</a>
+        				<a class="button" v-on:click="activeLocation.flow.cancel()">
+        				    <span class="icon icon-close"></span>
+        				<span>{{ t('flowupload', 'Cancel') }}</span>
+        				</a>
+        				<a id="hideFinishedButton" class="button" v-on:click="hideFinished = !hideFinished">
+        				    <input type="checkbox" ng-model="hideFinished"></input>
+        				    <span>{{ t('flowupload', 'Hide finished uploads') }}</span>
+        				</a>
+        			</div>
+        			<hr>
+        			<p>
+        				<span class="label">{{ t('flowupload', 'Size') + ' : ' /*+ activeLocation.flow.getSize() | bytes*/ }}</span>
+        				<span class="label" v-if="activeLocation.flow.getFilesCount() != 0">{{ t('flowupload', 'Progress') + ' : ' + trimDecimals(activeLocation.flow.progress()*100, 2) + '%'}}</span>
+        				<span class="label" v-if="activeLocation.flow.isUploading()">{{ t('flowupload', 'Time remaining') + ' : ' + activeLocation.flow.timeRemaining() | seconds }}</span>
+        				<span class="label" v-if="activeLocation.flow.isUploading()">{{ t('flowupload', 'Uploading') + '...' }}</span>
+        			</p>
+        			<hr>
+        			<table id="uploadsTable">
+        				<thead>
+        					<tr>
+        						<th class="hideOnMobile" style="width:5%">
+        							<span class="noselect">#</span>
+        						</th>
+        						<th ng-click="tableSortClicked('relativePath')">
+        							<a class="noselect">
+        							<span>{{ t('flowupload', 'Name') }}</span>
+        							<span ng-class="{'icon-triangle-n':  (sortType == 'relativePath' && sortReverse), 'icon-triangle-s': (sortType == 'relativePath' && !sortReverse)}" class="sortIndicator"></span>
+        							</a>
+        						</th>
+        						<th></th>
+        						<th class="hideOnMobile" ng-click="tableSortClicked('-currentSpeed')" style="width:10%">
+        							<a class="noselect">
+        							<span>{{ t('flowupload', 'Uploadspeed') }}</span>
+        							<span ng-class="{'icon-triangle-n':  (sortType == '-currentSpeed' && sortReverse), 'icon-triangle-s': (sortType == '-currentSpeed' && !sortReverse)}" class="sortIndicator"></span>
+        							</a>
+        						</th>
+        						<th ng-click="tableSortClicked('-size')" style="width:10%">
+        							<a class="noselect">
+        							<span>{{ t('flowupload', 'Size') }}</span>
+        							<span ng-class="{'icon-triangle-n':  (sortType == '-size' && sortReverse), 'icon-triangle-s': (sortType == '-size' && !sortReverse)}" class="sortIndicator"></span>
+        							</a>
+        						</th>
+        						<th ng-click="tableSortClicked('-progress()')" style="width:20%">
+        							<a class="noselect">
+        							<span>{{ t('flowupload', 'Progress') }}</span>
+        							<span ng-class="{'icon-triangle-n':  (sortType == '-progress()' && sortReverse), 'icon-triangle-s': (sortType == '-progress()' && !sortReverse)}" class="sortIndicator"></span>
+        							</a>
+        						</th>
+        					</tr>
+        				</thead>
+        				<tbody>
+        					<tr v-if="!(file.isComplete() && hideFinished)" v-for="file in filteredFiles">
+        						<td class="hideOnMobile">{{$index+1}}</td>
+        						<td class="ellipsis" v-bind:title="'UID: ' + file.uniqueIdentifier">
+        							<span>{{file.relativePath}}</span>
+        						</td>
+        						<td>
+        							<div class="actions" v-if="!file.isComplete() || file.error">
+        								<a class="action permanent" v-bind:title="t('flowupload', 'Resume')" v-click="file.resume()" v-if="!file.isUploading() && !file.error">
+        								<span class="icon icon-play"></span>
+        								</a>
+        								<a class="action permanent" v-bind:title="t('flowupload', 'Pause')" v-click="file.pause()" v-if="file.isUploading() && !file.error">
+        								<span class="icon icon-pause"></span>
+        								</a>
+        								<a class="action permanent" v-bind:title="t('flowupload', 'Retry')" v-click="file.retry()" v-show="file.error">
+        								<span class="icon icon-play"></span>
+        								</a>
+        								<a class="action permanent" v-bind:title="t('flowupload', 'Cancel')" v-click="file.cancel()">
+        								<span class="icon icon-close"></span>
+        								</a>
+        							</div>
+        						</td>
+        						<td class="hideOnMobile">
+        							<span v-if="file.isUploading()">{{file.currentSpeed | byterate}}</span>
+        						</td>
+        						<td title="'Chunks: ' + file | completedChunks + ' / ' + file.chunks.length">
+        							<span class="hideOnMobile" v-if="!file.isComplete()">{{file.size*file.progress() | bytes}}/</span>
+        							<span>{{file.size | bytes}}</span>
+        						</td>
+        						<td>
+        							<progress v-if="!file.isComplete() && !file.error" class="progressbar hideOnMobile" max="1" v-bind:value="file.progress()"></progress>
+        							<span v-if="!file.isComplete() && !file.error">{{ trimDecimals(file.progress()*100, 2) }}%</span>
+        							<span v-if="file.isComplete() && !file.error">{{ t('flowupload', 'Completed') }}</span>
+        							<span v-if="file.error">{{ t('flowupload', 'Error') }}</span>
+        						</td>
+        					</tr>
+        				</tbody>
+        			</table>
+        			<p id="homeDirectoryLink"><a href="../files?dir=%2Fflowupload">{{ t('flowupload', 'The files will be saved in your home directory.') }}</a></p>
+        		</div>
+        	</div>
+    	</AppContent>
+    </Content>
 </template>
 
 <script>
@@ -222,11 +247,12 @@ export default {
 	},
 	data: function() {
 		return {
-			loading: false,
+			loading: true,
 			locations: [],
             baseUrl: OC.generateUrl('/apps/flowupload'),
             currentLocation: undefined,
-            hideFinished: false
+            hideFinished: false,
+            activeLocationId: false,
 		}
 	},
     mounted: function (){
@@ -238,6 +264,8 @@ export default {
             }else{
                 console.log("no starred locations available");
             }
+            
+            self.loading = false;
         });
     },
     methods: {
